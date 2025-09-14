895d5efd40b8fef58a9f716fb4305722ed410fc8
Raj Yaduvanshi <132116298+razz56@users.noreply.github.com>
2025-09-14 17:42:06 +0530

    Update and rename htmlsourcecode.sh to Empty.txt

--- htmlsourcecode.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p headers responsebody
-CURRENT_PATH=$(pwd)
-for x in $(cat "$1"); do
-    NAME=$(echo "$x" | awk -F/ '{print $3}')
-    curl -s -I -H "X-Forwarded-For: evil.com" --connect-timeout 10 "$x" > "$CURRENT_PATH/headers/$NAME"
-    curl -s -L -H "X-Forwarded-For: evil.com" --connect-timeout 10 "$x" > "$CURRENT_PATH/responsebody/$NAME"
-done
